07d6107e3e1b9bff284c25a57d513972980751f1
Yuvraj-S-Lodhi <23b0676@iitb.ac.in>
2024-06-26 19:37:42 +0530

    Add files via upload

---
+++ block.py
@@ -0,0 +1,12 @@
+import hashlib
+import time
+import requests
+
+class Block:
+    def __init__(self, index, timestamp, vote_info, proof, previous_hash):
+        """ This initializes a block with index, timestamp, vote information, proof, and previous hash."""
+        pass
+
+    def compute_hash(self):
+        """ This function computes the hash of the block."""
+        pass
